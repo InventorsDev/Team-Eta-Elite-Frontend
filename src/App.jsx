99994dc61e715947d91d2ade130ccfc380ce0a65
daniel_eln <omoniyiopemipo6@gmail.com>
2025-08-02 08:58:49 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  return (
+    <div className='flex h-screen w-full flex-col gap-6 justify-center items-center bg-neutral-800'>
+      <div className=''>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo size-30" alt="Vite logo" />
+        </a>
+      </div>
+      <h1 className='text-amber-400 font-extrabold text-3xl sm:text-5xl'>Vite + React</h1>
+    </div>
+  )
+}
+
+export default App
