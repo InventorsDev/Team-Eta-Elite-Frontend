0eb5edc0cb2a0739ef3060974a22ea9f9bcea063
daniel_eln <omoniyiopemipo6@gmail.com>
2025-08-13 14:04:34 +0100

    feat: build reusable Table component - build Products Page for vendor

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,7 +3,7 @@ const Button = (props) => {
     return (
         <button
             className={`
-                p-3 w-full rounded text-center cursor-pointer ease-transition active:scale-95 disabled:opacity-50 disabled:cursor-not-allowed
+                p-3 w-full sm:w-fit sm:px-4 rounded text-center cursor-pointer ease-transition active:scale-95 disabled:opacity-50 disabled:cursor-not-allowed
                 ${type === "bg-black" 
                     ? "text-white bg-[var(--primary-color)] hover:bg-gray-900"
                     : "text-[var(--primary-color)] border border-[var(--primary-color)] hover:text-white hover:bg-[var(--primary-color)]"
