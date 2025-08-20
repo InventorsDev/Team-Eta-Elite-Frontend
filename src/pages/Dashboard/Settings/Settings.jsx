5c28253fff23ae0d6106e20ce07516bcf48f0e97
Trumptomz <gbadamosioluwatomiwa441@gmail.com>
2025-08-20 22:08:10 +0100

    UI improved

--- src/pages/Dashboard/Settings/Settings.jsx
+++ src/pages/Dashboard/Settings/Settings.jsx
@@ -288,10 +288,10 @@ const Settings = () => {
                         </button>
                     </div>
                     {showConfirm && (
-                        <div className="fixed z-1000 w-full overflow-hidden inset-0 flex items-center justify-center backdrop-blur-sm  bg-opacity-20">
-                            <div className="bg-white border-1 border-gray-200 py-10 px-8 rounded-[10px]  shadow-lg w-[90%] md:w-[500px]">
+                        <div className="fixed z-1000 w-full overflow-hidden inset-0 flex items-center justify-center backdrop-blur-sm  bg-[var(--primary-color)]/10">
+                            <div className="border-1 bg-white border-gray-200 py-10 px-8 rounded-[10px]  shadow-gray-400 shadow-lg w-[90%] md:w-[500px]">
                                 <div className="font-semibold flex flex-col justify-center items-center text-center  mb-4">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="Red" className="size-14">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="Red" className="size-15">
                                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z" />
                                     </svg>
 
