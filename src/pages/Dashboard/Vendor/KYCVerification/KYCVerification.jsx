271d4c670976b74084ea01fceda9fe4aa5a0feff
Trumptomz <gbadamosioluwatomiwa441@gmail.com>
2025-08-26 12:09:48 +0100

    Feature: place Add Account form in vendor KYC page

--- src/pages/Dashboard/Vendor/KYCVerification/KYCVerification.jsx
+++ src/pages/Dashboard/Vendor/KYCVerification/KYCVerification.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ImageUpload from "../../../../components/ImageUpload/ImageUpload";
 import Toast from "../../../../components/Toast/Toast";
 import bellIcon from "../../../../assets/imagenotification.svg";
@@ -8,10 +8,44 @@ const KYCVerification = () => {
   const [files, setFiles] = useState({ idFront: null, idBack: null });
   const [status, setStatus] = useState("Pending"); // Pending | Approved | Rejected
   const [toast, setToast] = useState(null);
+  const [accountName, setaccountName] = useState("");
+  const [accountNumber, setaccountNumber] = useState("");
+  const [bankName, setbankName] = useState("First Bank");
+  const [accountError, setAccountError] = useState("");
+
 
   const handleSelect = (name, file) => {
     setFiles((prev) => ({ ...prev, [name]: file }));
   };
+
+  useEffect(() => {
+      
+      if (accountNumber.length === 0) {
+          setAccountError("");
+      } else if (!/^\d+$/.test(accountNumber)) {
+          setAccountError("Account number must be only numbers");
+      } else if (accountNumber.length !== 10) {
+          setAccountError("Account number must be 10 digits");
+      } else {
+          setAccountError("");
+      }
+  }, [accountNumber]);
+
+  const handleAccountSubmit = (e) => {
+    e.preventDefault();
+    if (accountError) {
+        setToast({
+            message: "Please fix the account number error before submitting",
+            type: "error",
+        });   
+        return;
+    }
+    // Proceed with API call to add account
+    setToast({
+        message: "Bank account added successfully",
+        type: "success",
+    });
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -83,6 +117,68 @@ const KYCVerification = () => {
           <span className="font-semibold">{status}</span>
         </span>
       </div>
+
+      {/* Bank Account Form */}
+      <form onSubmit={handleAccountSubmit}  className="mt-10 mb-10 bg-white shadow rounded-xl p-6 space-y-6">
+        <h2 className="text-[22px] font-semibold">Add Bank Account</h2>
+        <div className="mt-7">
+            <label htmlFor="account-name" 
+                className="text-[#000000CC] font-semibold block mb-2">
+                Account Holder Name
+            </label>
+            <input type="name" 
+                id="account-name" 
+                className="border-1  border-[#00000080] p-2 w-full rounded-[10px]" 
+                placeholder="Account full name" 
+                value={accountName}
+                onChange={(e) =>setaccountName(e.target.value)}
+                required 
+            />
+        </div>
+
+        <div className="mt-7">
+            <label htmlFor="account-name" 
+                className="text-[#000000CC] font-semibold block mb-2">
+                Bank Name
+            </label>
+            <select type="name" 
+                id="bank-name" 
+                className="border-1  border-[#00000080] p-2 w-full rounded-[10px]" 
+                placeholder="Account full name" 
+                value={bankName}
+                onChange={(e) =>setbankName(e.target.value)}
+                required >
+                {/* <option value="" disabled>Select Bank</option> */}
+                <option className="border-1 rounded-2xl" value="First Bank">First Bank</option>
+            </select>
+        </div>
+
+        {accountError && (
+            <span className="transition font-medium text-red-500 ">{accountError}</span>
+        )}
+        <div className="mt-7">
+            <label htmlFor="account-number" 
+                className="text-[#000000CC] font-semibold block mb-2">
+                Account Number
+            </label>
+            <input type="numb" 
+                id="account-number" 
+                className="border-1  border-[#00000080] p-2 w-full rounded-[10px]" 
+                placeholder="0123456789"
+                value={accountNumber}
+                onChange={(e) =>setaccountNumber(e.target.value)} 
+                required 
+            />
+        </div>
+        <div className="flex w-full mt-5 p-2">
+            <button
+                className="bg-[var(--primary-color)] active:scale-95 cursor-pointer rounded-[10px] text-white ml-auto px-12 py-3  hover:bg-[var(--primary-color)] transition-all duration-300"
+                type="submit"
+            >
+                Add Account
+            </button>
+        </div>
+      </form>
 
       {/* Upload Form */}
       <form
