a54e35bf4b62ff51f0f54e3369e2be7df984cafe
daniel_eln <omoniyiopemipo6@gmail.com>
2025-09-01 06:56:32 +0100

    feat: place AddBankAccount form to its own component, fetch orders count by vendor_id

---
+++ src/components/AddBank/AddBank.jsx
@@ -0,0 +1,115 @@
+import { useState, useEffect } from 'react';
+import Toast from '../Toast/Toast';
+
+const AddBank = () => {
+    const [toast, setToast] = useState(null);
+    const [accountName, setaccountName] = useState("");
+    const [accountNumber, setaccountNumber] = useState("");
+    const [bankName, setbankName] = useState("First Bank");
+    const [accountError, setAccountError] = useState("");
+
+    useEffect(() => {
+      if (accountNumber.length === 0) {
+        setAccountError("");
+      } else if (!/^\d+$/.test(accountNumber)) {
+        setAccountError("Account number must be only numbers");
+      } else if (accountNumber.length !== 10) {
+        setAccountError("Account number must be 10 digits");
+      } else {
+        setAccountError("");
+      }
+    }, [accountNumber]);
+
+    const handleAccountSubmit = (e) => {
+        e.preventDefault();
+        if (accountError) {
+            setToast({
+                message: "Please fix the account number error before submitting",
+                type: "error",
+            });   
+            return;
+        }
+        // Proceed with API call to add account
+        setToast({
+            message: "Bank account added successfully",
+            type: "success",
+        });
+    }
+
+    return (
+      <form onSubmit={handleAccountSubmit}  className="mt-10 mb-10 bg-white shadow rounded-xl p-6 space-y-6">
+        {toast && (
+            <Toast 
+                type={toast.type}
+                message={toast.message}
+                onClose={() => setToast(null)}
+            />
+        )}
+
+        <h2 className="text-[22px] font-semibold">Add Bank Account</h2>
+        <div className="mt-7">
+            <label htmlFor="account-name" 
+                className="text-[#000000CC] font-semibold block mb-2">
+                Account Holder Name
+            </label>
+            <input type="name" 
+                id="account-name" 
+                className="border-1  border-[#00000080] p-2 w-full rounded-[10px]" 
+                placeholder="Account full name" 
+                value={accountName}
+                onChange={(e) =>setaccountName(e.target.value)}
+                required 
+            />
+        </div>
+
+        <div className="mt-7">
+            <label htmlFor="account-name" 
+                className="text-[#000000CC] font-semibold block mb-2">
+                Bank Name
+            </label>
+            <select type="name" 
+                id="bank-name" 
+                className="border-1  border-[#00000080] p-2 w-full rounded-[10px]" 
+                placeholder="Account full name" 
+                value={bankName}
+                onChange={(e) =>setbankName(e.target.value)}
+                defaultValue={""}
+                required 
+            >
+                <option value="">Select Bank</option>
+                <option className="border-1 rounded-2xl" value="First Bank">First Bank</option>
+            </select>
+        </div>
+
+        <div className="mt-7">
+            <label htmlFor="account-number" 
+                className="text-[#000000CC] font-semibold block mb-2">
+                Account Number
+            </label>
+            <input type="text" 
+                id="account-number" 
+                maxLength={10}
+                className="border-1  border-[#00000080] p-2 w-full rounded-[10px]" 
+                placeholder="0123456789"
+                value={accountNumber}
+                onChange={(e) =>setaccountNumber(e.target.value)} 
+                required 
+            />
+        </div>
+        <div className="flex w-full mt-5 p-2">
+            <button
+                className="bg-[var(--primary-color)] active:scale-95 cursor-pointer rounded-[10px] text-white ml-auto px-12 py-3  hover:bg-[var(--primary-color)] transition-all duration-300"
+                type="submit"
+            >
+                Add Account
+            </button>
+        </div>
+        
+        {accountError && (
+            <span className="transition font-medium text-red-500 ">{accountError}</span>
+        )}
+      </form>
+    );
+}
+
+export default AddBank;
--- src/pages/Dashboard/Vendor/Overview/Overview.jsx
+++ src/pages/Dashboard/Vendor/Overview/Overview.jsx
@@ -48,7 +48,8 @@ const Overview = () => {
         try {
             const { count, error } = await supabase
                 .from('orders')
-                .select('*', { count: 'exact', head: true });
+                .select('*', { count: 'exact', head: true })
+                .eq("vendor_id", vendorId);
     
             if (!error) setOverviewData(prev => ({...prev, orders: count}));
         } catch (error) {
@@ -63,9 +64,9 @@ const Overview = () => {
 
     const getAmount = (title) => {
         switch(title) {
-            case "Products Listed": 
+            case "Products": 
                 return Number(overviewData.products);
-            case "Orders Received":
+            case "Orders":
                 return Number(overviewData.orders);
             case "Total Earned":
                 return formatToNaira(Number(overviewData.totalEarned));
@@ -78,7 +79,7 @@ const Overview = () => {
 
     const salesDiv =[
         {
-            Title:"Products Listed",
+            Title:"Products",
             amount: 0,
             icon:(<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="text-[#310EB7] size-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" /></svg>
@@ -86,7 +87,7 @@ const Overview = () => {
             color: "#135CEE33"
         },
         {
-            Title:"Orders Received",
+            Title:"Orders",
             amount: 0,
             icon: (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="text-[#2FA211] size-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M9 3.75H6.912a2.25 2.25 0 0 0-2.15 1.588L2.35 13.177a2.25 2.25 0 0 0-.1.661V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 0 0-2.15-1.588H15M2.25 13.5h3.86a2.25 2.25 0 0 1 2.012 1.244l.256.512a2.25 2.25 0 0 0 2.013 1.244h3.218a2.25 2.25 0 0 0 2.013-1.244l.256-.512a2.25 2.25 0 0 1 2.013-1.244h3.859M12 3v8.25m0 0-3-3m3 3 3-3" />
